9e45c23bf12171cb5399e86e5a71121eeba3dabc
THEXTREMESPECTR <93404684+THEXTREMESPECTR@users.noreply.github.com>
2022-01-09 10:34:09 -0600

    Add files via upload

---
+++ randomengine.js
@@ -0,0 +1,7 @@
+
+var play = ["Valorant","Minecraft","Legend of Zelda"]
+function function1(){
+var rnd = Math.floor(Math.random()*play.length);
+//document.getElementById("messages001")//.innerHTML = play[rnd]
+  alert(play[rnd]);
+}
